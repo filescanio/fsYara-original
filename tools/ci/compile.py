b1114cc09fc1f949bb9bcf99ab831fc1524f67b3
Roland Győrffi <roland.gyorffi@opswat.com>
2025-02-06 12:02:51 +0100

    Updated compiler script

--- tools/ci/compile.py
+++ tools/ci/compile.py
@@ -3,21 +3,25 @@ import pathlib
 import sys
 
 tmpfile = pathlib.Path('temp.bin')
-cmd = ['yarac'] + sys.argv[1:] + [tmpfile.absolute()]
+cmd = ['yarac'] + sys.argv[1:] + [str(tmpfile.absolute())]
 
 ERROR = -1
 try:
     process = subprocess.run(cmd, check=False)
     ERROR = process.returncode
+
+    if not tmpfile.exists():
+        print(f'[COMPILER] compiled file wasnt created: {str(tmpfile)}')
+        ERROR = -2
 except Exception as e:
     print(f'[COMPILER] Process Exception happened: {e}')
 
 try:
     tmpfile.unlink()
-except FileNotFoundError as e:
-    if ERROR == 0:
-        ERROR = -2
 except Exception as e:
-    print(f'[COMPILER] File Exception happened: {e}')
+    print(f'[COMPILER] Deletion Exception happened: {e}')
+
+if ERROR != 0:
+    print(f'[COMPILER] returned error! cmd: {cmd}')
 
 sys.exit(ERROR)
