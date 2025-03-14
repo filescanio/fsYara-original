33d7694eaed2322ad5f71ae73e7b844e10e7e053
zsoltmajoropswat <zsolt.major@opswat.com>
2025-03-14 10:49:07 +0100

    Add scripts for creating and comparing Yara matches; update workflow for rule testing

---
+++ tools/ci/compare_matches.py
@@ -0,0 +1,49 @@
+import argparse
+import json
+from pathlib import Path
+
+
+def check_matches(o: Path, h: Path):
+    original = json.load(open(o))
+    hardened = json.load(open(h))
+
+    print("> Create errors list")
+    errors: dict = dict()
+    for original_rule, original_matches in original.items():
+        hardened_matches = hardened.get(original_rule, [])
+        for original_match in original_matches:
+            if original_match not in hardened_matches:
+                append_error(errors, original_rule, original_match, "missing")
+
+    for hardened_rule, hardened_matches in hardened.items():
+        original_matches = original.get(hardened_rule, [])
+        for hardened_match in hardened_matches:
+            if hardened_match not in original_matches:
+                append_error(errors, hardened_rule, hardened_match, "extra")
+
+    print("> Write errors list")
+    write_json(errors, "errors.json")
+    assert not errors
+
+
+def append_error(errors: dict, rule: str, match: str, error_type: str):
+    if rule not in errors:
+        errors[rule] = dict()
+    if error_type not in errors[rule]:
+        errors[rule][error_type] = []
+
+    errors[rule][error_type].append(match)
+
+
+def write_json(matches: dict, filename: str):
+    with open(filename, "w") as f:
+        json.dump(matches, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--original", "-o", type=Path, required=True)
+    parser.add_argument("--hardened", "-H", type=Path, required=True)
+    args = parser.parse_args()
+
+    check_matches(args.original, args.hardened)
---
+++ tools/ci/create_matches.py
@@ -0,0 +1,76 @@
+import argparse
+from collections import defaultdict
+import json
+from posixpath import basename
+from typing import Iterator
+import zipfile
+from concurrent.futures import ThreadPoolExecutor as tpe
+from boto3.session import Session
+
+import yara
+
+from pathlib import Path
+
+
+def download_samples() -> Path:
+    tmp_dir = Path("tmp/matchingsamples")
+    if not tmp_dir.exists():
+        tmp_dir.mkdir(parents=True, exist_ok=True)
+
+        print("> Downloading samples")
+        session = Session()
+        client = session.client(service_name="s3")
+        zip = Path("tmp/samples.zip")
+        client.download_file("yara-matching-samples", "matchingsamples.zip", zip)
+
+        print("> Extracting samples")
+        with zipfile.ZipFile(zip, "r") as zip_ref:
+            zip_ref.extractall(tmp_dir, pwd=b"infected")
+
+    return tmp_dir
+
+
+def get_yara_files(yara_dir: Path) -> list[Path]:
+    return list(yara_dir.rglob("**/*.yar")) + list(yara_dir.rglob("**/*.yara"))
+
+
+def get_yara_matches(yara_path: Path, samples_path: Path) -> dict[str, list[str]]:
+    yara_files = get_yara_files(yara_path)
+    samples = list(samples_path.rglob("*"))
+
+    def matches(yara_file: Path) -> dict:
+        result = defaultdict(list)
+        compiled = yara.compile(str(yara_file))
+        for sample in samples:
+            matches = compiled.match(str(sample))
+            for m in matches:
+                result[str(m)].append(basename(sample))
+        return result
+
+    def partial_results() -> Iterator[dict]:
+        with tpe(max_workers=10) as executor:
+            return executor.map(matches, yara_files)
+
+    final_matches: dict[str, list[str]] = defaultdict(list)
+    for partial_result in partial_results():
+        for rule, partial_matches in partial_result.items():
+            final_matches[rule].extend(partial_matches)
+    return final_matches
+
+
+def write_json(matches: dict, filename: str):
+    with open(filename, "w") as f:
+        json.dump(matches, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", "-p", type=Path, default="./")
+    parser.add_argument("--output", "-o", type=Path, required=True)
+    args = parser.parse_args()
+
+    samples = download_samples()
+    print("> Searching for matches")
+    matches = get_yara_matches(args.path, samples)
+    print("> Writing matches into", args.output)
+    write_json(matches, args.output)
